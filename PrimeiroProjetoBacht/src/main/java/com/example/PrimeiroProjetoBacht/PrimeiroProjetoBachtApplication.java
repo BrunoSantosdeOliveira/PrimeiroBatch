416d01e34112eacc3d806676e59354f716582f9f
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2022-05-31 01:14:59 -0300

    primeiroCommit

---
+++ PrimeiroProjetoBacht/src/main/java/com/example/PrimeiroProjetoBacht/PrimeiroProjetoBachtApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrimeiroProjetoBacht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoBachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoBachtApplication.class, args);
+	}
+
+}
---
+++ PrimeiroProjetoBacht/src/test/java/com/example/PrimeiroProjetoBacht/PrimeiroProjetoBachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrimeiroProjetoBacht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoBachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
